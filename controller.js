000309bf3ac9b13d309fe989d82003a4ef355e24
Lucas Albuquerque de Almeida <lucas.ufcg@gmail.com>
2015-10-15 23:04:21 -0300

    Hangman Challenge

---
+++ controller.js
@@ -0,0 +1,40 @@
+angular.module('app').controller('controller',['$scope','$rootScope','service',function($scope,$rootScope,service){
+
+	var self = this;
+	/**
+	 * This commands represents the hangaman commands:
+	 * 1 - startGame / 2 - nextWord / 3 - nextWord / 4 - getResult / 5 - submitResult
+	 */
+	self.command = undefined;
+	
+	$scope.sessionID = undefined;
+	$scope.serverOut;
+
+	$scope.startGame = function(){
+		service.startGame().then(function(sucess){
+			$scope.serverOut = sucess;
+		});
+	};
+
+	/* Give a word for the player */
+	$scope.nextWord = function(){
+		if($scope.serverOut.sessionId === undefined){
+			alert("Ops! sessionId invalid!");
+			return;
+		}
+		service.nextWord($scope.serverOut.sessionId).then(function(sucess){
+			$scope.serverOut = sucess;	
+		});
+	};
+
+	$scope.guess = function(){
+		if($scope.serverOut.sessionId === undefined){
+			alert("Ops! sessionId invalid!");
+			return;
+		}
+		service.guess($scope.serverOut.sessionId, "P").then(function(sucess){
+			$scope.serverOut = sucess;	
+		});
+	}
+
+}]);
---
+++ service.js
@@ -0,0 +1,34 @@
+angular.module('app').service('service',['$http',function($http){
+
+	var playerId = "lucas.ufcg@gmail.com";
+	var RESQUEST_URL = "https://strikingly-hangman.herokuapp.com/game/on";
+	
+	var config = {headers:{"Content-Type": "application/json","playerId":playerId}};
+
+	
+	/* Start the Game */
+	this.startGame = function(){
+		var data  = {"playerId":playerId, "action":"startGame"};
+		return $http.post(RESQUEST_URL, data, config).then(function(response){
+			console.log("--------- START GAME",response.data);
+			return response.data;
+		});
+	};
+
+	this.nextWord = function(sessionId){
+		var data = {"sessionId":sessionId, "action":"nextWord"};
+		return $http.post(RESQUEST_URL, data, config).then(function(response){
+			console.log("--------- GIVE ME A WORD",response.data);
+			return response.data;
+		});
+	};
+
+	this.guess = function(sessionId,letter){
+		var data = {"sessionId":sessionId, "action":"nextWord","guess":letter};
+		return $http.post(RESQUEST_URL, data, config).then(function(response){
+			console.log("--------- MAKE A GUESS",response.data);
+			return response.data;
+		});
+	};
+
+}]);
